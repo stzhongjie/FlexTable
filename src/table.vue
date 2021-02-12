7520609eea89ba7ff066fb38f94dbe3ee39470fc
zzj <zhangzhongjie@shein.com>
2021-02-12 23:28:49 +0800

    测试

--- src/table.vue
+++ src/table.vue
@@ -467,11 +467,10 @@ export default {
                     console.log('进来')
                     this.doLayout();
                     this.initData();
-
+                    setTimeout(() => {
+                        this.reSetItemHeight();
+                    }, 0)
                 }
-                // setTimeout(() => {
-                //     this.reSetItemHeight();
-                // }, 0)
 
             },
             deep: true,
